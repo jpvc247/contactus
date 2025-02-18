b0671b146e9c29daf39f012add0aaae09c78d094
jpvc247 <cekpo36@gmail.com>
2025-02-18 10:29:40 +0100

    Implement radio buttons in input field and make the form responsive

--- src/ContactForm.jsx
+++ src/ContactForm.jsx
@@ -1,78 +1,110 @@
 import { Button } from "react-bootstrap";
+import { useState } from "react";
 
 function ContactForm() {
+  const [selected, setSelected] = useState(null);
   return (
     <>
       <h1>Contact Us</h1>
       <form>
-        <div className="column-wrapper">
-          <div className="column-wrapper-item col-sm-6">
+        <div className="row">
+          <div className="col-12 col-md-6 mb-3">
             <label className="form-label">
               First Name<span>*</span>
             </label>
-            <br />
             <input type="text" className="form-control" />
           </div>
-          <div className="column-wrapper-item col-sm-6">
+          <div className="col-12 col-md-6 mb-3">
             <label className="form-label">
               Last Name<span>*</span>
             </label>
-            <br />
             <input type="text" className="form-control" />
           </div>
         </div>
-        <br />
-        <div className="column-wrapper-item">
+
+        <div className="col-12 mb-3">
           <label className="form-label">
             Email Address<span>*</span>
           </label>
-          <br />
           <input type="text" className="form-control" />
         </div>
-        <br />
 
-        <div>
+        <div className="col-12">
           <label className="form-label">
             Query Type<span>*</span>
           </label>
-          <br />
         </div>
 
-        <div className="column-wrapper">
-          <div className="column-wrapper-item col-sm-6">
-            <input type="text" className="form-control" />
+        <div className="row">
+          {/* First Input with Radio */}
+          <div className="col-12 col-md-6 mb-3 position-relative">
+            <div className="position-relative">
+              <input
+                type="text"
+                className="form-control ps-5" // Left padding for radio button
+                disabled
+                placeholder="General Enquiry"
+              />
+              <div className="position-absolute start-0 top-50 translate-middle-y d-flex align-items-center ps-3" style={{ height: "100%" }}>
+                <input
+                  type="radio"
+                  name="query"
+                  checked={selected === "General Enquiry"}
+                  onChange={() => setSelected("General Enquiry")}
+                  className="form-check-input"
+                  style={{ width: "1.2rem", height: "1.2rem" }} // Adjusted size
+                />
+              </div>
+            </div>
           </div>
-          <div className="column-wrapper-item col-sm-6">
-            <input type="text" className="form-control" />
+
+          {/* Second Input with Radio */}
+          <div className="col-12 col-md-6 position-relative">
+            <div className="position-relative">
+              <input
+                type="text"
+                className="form-control ps-5"
+                disabled
+                placeholder="Support Request"
+              />
+              <div className="position-absolute start-0 top-50 translate-middle-y d-flex align-items-center ps-3" style={{ height: "100%" }}>
+                <input
+                  type="radio"
+                  name="query"
+                  checked={selected === "Support Request"}
+                  onChange={() => setSelected("Support Request")}
+                  className="form-check-input"
+                  style={{ width: "1.2rem", height: "1.2rem" }}
+                />
+              </div>
+            </div>
           </div>
         </div>
-        <br />
 
-        <div className="column-wrapper-item">
+        <div className="col-12 mb-2">
           <label className="form-label">
             Message<span>*</span>
           </label>
-          <br />
           <textarea className="form-control no-resize"></textarea>
         </div>
-        <br />
-        <div className="column-wrapper">
-          <div className="column-wrapper-item">
+
+        <div className="d-flex align-items-center">
+          <div className="me-2">
             <input type="checkbox" />
           </div>
-
-          <div className="column-wrapper-item">
-            <p>
-              I concent to being contacted by the team<span>*</span>
-            </p>
+          <div className="me-2 mt-3">
+          <p>
+            I consent to being contacted by the team<span>*</span>
+          </p>
           </div>
+          
+          
         </div>
-        <div className="column-wrapper">
-          <div className="column-wrapper-item col-12">
-            <Button variant="success" className="w-100">
-              Submit
-            </Button>
-          </div>
+
+        <div className="col-12">
+          <Button variant="success" className="w-100">
+            Submit
+          </Button>
         </div>
       </form>
     </>
